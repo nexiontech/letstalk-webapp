d2ae1f0445e0328c1cd85532644ca1be98c0c7af
Thando12345 <69775289+Thando12345@users.noreply.github.com>
2025-04-02 13:24:43 +0200

    implemented scalable component architecture

---
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+// Styles moved to styles.css
+
+function NavigationBar() {
+    return (
+        <header className="site-header">
+            <nav className="navbar" aria-label="Main navigation">
+                <div className="nav-left">
+             
+                
+              
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default NavigationBar;
---
+++ src/pages/ForgotPasswordPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ForgotPasswordForm from '../components/AuthForms/ForgotPasswordForm';
+
+function ForgotPasswordPage() {
+    return (
+        <div>
+            <ForgotPasswordForm />
+        </div>
+    );
+}
+
+export default ForgotPasswordPage;
